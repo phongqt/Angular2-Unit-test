48b24c613bc109ed62c17e4af9e366050c21ffed
gg_phongtv <phongtv@greenglobal.vn>
2016-03-08 17:22:14 +0700

    unit test with angular 2 typescript

---
+++ test/test-class.js
@@ -0,0 +1,11 @@
+describe('Hero', function () {
+    it('has name given in the constructor', function () {
+        var hero = new Hero(1, 'Super Cat');
+        expect(hero.name).toEqual('Super Cat');
+    });
+    it('has the id given in the constructor', function () {
+        var hero = new Hero(1, 'Super Cat');
+        expect(hero.id).toEqual(1);
+    });
+});
+//# sourceMappingURL=test-class.js.map
