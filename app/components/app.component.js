dd31cf40b66d5f12b32b1e24640d9e18dfe4f9ae
gg_phongtv <phongtv@greenglobal.vn>
2016-03-09 16:23:27 +0700

    research - unit test with karma, jasmine

--- app/components/app.component.js
+++ app/components/app.component.js
@@ -1,6 +1,4 @@
-System.register(["angular2/core", 'angular2/router', "./home.component"], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
+System.register(["angular2/core", 'angular2/router', "./home.component"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -47,7 +45,7 @@ System.register(["angular2/core", 'angular2/router', "./home.component"], functi
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
-            }());
+            })();
             exports_1("AppComponent", AppComponent);
         }
     }
